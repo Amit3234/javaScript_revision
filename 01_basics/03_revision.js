375325cacc2510c99be6ccee3fe45f5938238f70
Amit <123800811+Amit3234@users.noreply.github.com>
2023-10-30 19:05:45 +0000

    javascipt Prefix increment and Postfix increment

---
+++ 01_basics/03_revision.js
@@ -0,0 +1,6 @@
+//  Prefix increment and Postfix increment
+// JavaScript Incrementing and Decrementing Operators
+
+let x =  10;
+// console.log(++x)   // Prefix increment    output ==> 11
+// console.log(x++)   // Postfix increment   output ==> 10
