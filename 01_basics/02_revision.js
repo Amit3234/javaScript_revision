4672b646aaf333af9800c9377c9ef99679463c29
Amit <123800811+Amit3234@users.noreply.github.com>
2023-10-30 18:35:49 +0000

    some conversion exmaple

---
+++ 01_basics/02_revision.js
@@ -0,0 +1,32 @@
+// conversion  
+// exmaple 1 
+let ex1 = "22aa"
+// type checking 
+console.log(typeof ex1);
+console.log(typeof (ex1));
+
+let exx1 = Number(ex1)
+console.log(typeof exx1)
+console.log(exx1)
+
+
+// exmaple 2
+let ex2 = null
+// type checking 
+console.log(typeof ex2);
+console.log(typeof (ex2));
+
+let exx2 = Number(ex2)
+console.log(typeof exx2)
+console.log(exx2)
+
+
+// exmaple 3
+let ex3 = undefined
+// type checking 
+console.log(typeof ex3);
+console.log(typeof (ex3));
+
+let exx3 = Number(ex3)
+console.log(typeof exx3)
+console.log(exx3)
